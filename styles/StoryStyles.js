2654b486596030c7350fe758f8e695df1ff4429c
Shenis Osias <shenisosias@gmail.com>
2019-10-21 18:39:36 -0700

    Pushing Story and its style

---
+++ styles/StoryStyles.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+ 
+var styles = StyleSheet.create({
+    story: {
+      backgroundColor: "lightblue",
+    },
+    text: {
+      fontSize: 20,
+      padding: 30,
+      lineHeight: 30
+    },
+    switch: {
+      paddingTop: 20,
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center"
+    }
+});
+ 
+export default styles;
